61d9926f1661f87959d90d3b655d59bd766272c4
Xujie Li <willsonleo163@gmail.com>
2024-03-25 17:02:10 +0100

    no message

--- Assignment 1/assignment_problem2.sh
+++ Assignment 1/assignment_problem2.sh
@@ -8,4 +8,3 @@ lscpu | grep "Model name" || cat /proc/cpuinfo | grep "model name" | uniq
 # Print CPU Max Frequency
 echo "CPU Max Frequency:"
 lscpu | grep "CPU max MHz" || cat /proc/cpuinfo | grep "cpu MHz" | uniq
-
